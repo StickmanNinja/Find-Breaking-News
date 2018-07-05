a332435ddce9fbceec93063f7b1c8c1725dd96ac
Seth J. Connell <sethjordanconnell@gmail.com>
2018-07-04 19:05:51 -0500

    Made static files local and updated theme.

--- requestagent.py
+++ requestagent.py
@@ -1,4 +1,11 @@
 # Importing flask.
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
